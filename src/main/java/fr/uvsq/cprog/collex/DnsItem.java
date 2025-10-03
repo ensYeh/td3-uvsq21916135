f01c279df8b61cabe49a27a5df17ae485ba4b8d6
uvsq21916135 <72436739+uvsq21916135@users.noreply.github.com>
2025-10-03 11:24:53 +0200

    Modification du cinstructeur pour eviter les noms null

--- src/main/java/fr/uvsq/cprog/collex/DnsItem.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsItem.java
@@ -1,23 +1,26 @@
 package fr.uvsq.cprog.collex;
 
+import java.util.Objects;
+
 public class DnsItem {
-    private NomMachine nom;
-    private AdressIP ip;
-    
+    private final NomMachine nom;
+    private final AdressIP ip;
+
     public DnsItem(NomMachine nom, AdressIP ip) {
-        this.nom = nom;
-        this.ip = ip;
+        this.nom = Objects.requireNonNull(nom, "Le nom ne doit pas être vide");
+        this.ip = Objects.requireNonNull(ip, "L'ip ne doit pas être vide");
     }
 
-    public NomMachine getNom(){
+    public NomMachine getNom() {
         return this.nom;
     }
 
-    public AdressIP getIp(){
+    public AdressIP getIp() {
         return this.ip;
     }
 
-    public String toString(){
+    @Override
+    public String toString() {
         return this.ip + " " + this.nom;
     }
 }
