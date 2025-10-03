9b5ea75d143133aea051ebf2fa7d8e501d3a469e
uvsq21916135 <72436739+uvsq21916135@users.noreply.github.com>
2025-10-03 11:33:11 +0200

    Ajout de tests pour la classe DnsItem : vérification des exceptions pour les paramètres null et validation des getters.

--- src/test/java/fr/uvsq/cprog/collex/DnsItemTest.java
+++ src/test/java/fr/uvsq/cprog/collex/DnsItemTest.java
@@ -1,4 +1,30 @@
 package fr.uvsq.cprog.collex;
 
+import static org.junit.Assert.assertSame;
+
+import org.junit.Test;
+
 public class DnsItemTest {
+
+    @Test(expected = NullPointerException.class)
+    public void constructeurRefuseNomNull() {
+        AdressIP ip = new AdressIP();
+        new DnsItem(null, ip);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void constructeurRefuseIpNull() {
+        NomMachine nom = new NomMachine();
+        new DnsItem(nom, null);
+    }
+
+    @Test
+    public void gettersRendentLesObjetsPasses() {
+        NomMachine nom = new NomMachine();
+        AdressIP ip = new AdressIP();
+        DnsItem item = new DnsItem(nom, ip);
+
+        assertSame(nom, item.getNom());
+        assertSame(ip, item.getIp());
+    }
 }
