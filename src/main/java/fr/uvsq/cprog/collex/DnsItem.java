3fa44898266ec6e294c2b5dc7865a2ab3f504976
uvsq21916135 <72436739+uvsq21916135@users.noreply.github.com>
2025-10-03 11:08:11 +0200

    Implémentation de la class DnsItem

--- src/main/java/fr/uvsq/cprog/collex/DnsItem.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsItem.java
@@ -1,4 +1,23 @@
 package fr.uvsq.cprog.collex;
 
 public class DnsItem {
+    private NomMachine nom;
+    private AdressIP ip;
+    
+    public DnsItem(NomMachine nom, AdressIP ip) {
+        this.nom = nom;
+        this.ip = ip;
+    }
+
+    public NomMachine getNom(){
+        return this.nom;
+    }
+
+    public AdressIP getIp(){
+        return this.ip;
+    }
+
+    public String toString(){
+        return this.ip + " " + this.nom;
+    }
 }
