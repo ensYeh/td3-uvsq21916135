df9a78a4904e0adc8f6870ce0a2a05fced02fd1c
uvsq21916135 <72436739+uvsq21916135@users.noreply.github.com>
2025-10-03 11:47:08 +0200

    Création des tests pour la class AdressIP

--- src/test/java/fr/uvsq/cprog/collex/AdressIPTest.java
+++ src/test/java/fr/uvsq/cprog/collex/AdressIPTest.java
@@ -1,4 +1,50 @@
 package fr.uvsq.cprog.collex;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class AdressIPTest {
+
+    @Test(expected = NullPointerException.class)
+    public void constructeurStringRefuseNull() {
+        new AdressIP(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void formatInvalideMoinsDe4Octets() {
+        new AdressIP("192.168.1");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void formatInvalidePlusDe4Octets() {
+        new AdressIP("192.168.1.1.5");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void formatInvalideOctetNonNumerique() {
+        new AdressIP("192.abc.1.1");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void formatInvalideOctetHorsPlage() {
+        new AdressIP("256.168.1.1");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void formatInvalideZeroNonSignificatif() {
+        new AdressIP("192.168.01.1");
+    }
+
+    @Test
+    public void formatValideEtToString() {
+        AdressIP ip = new AdressIP("193.51.31.90");
+        assertEquals("193.51.31.90", ip.toString());
+    }
+
+    @Test
+    public void toStringVidePourConstructeurDefaut() {
+        AdressIP ip = new AdressIP();
+        assertEquals("", ip.toString());
+    }
 }
